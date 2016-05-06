65318d333c4673f895110d59f2c218eb8c0cb2db
Hesham Adel <heshascorpe55@gmail.com>
2016-05-06 17:50:05 +0000

    sanity checks

--- public/register.php
+++ public/register.php
@@ -10,13 +10,36 @@
     }
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-          // 1. make sure that username is not empty 
-          // 2. make sure that password is not empty 
-          // 3. make sure that password and confirm password match
+        // get all the data from the user 
+        $username = $_POST["username"]; 
+        $password = $_POST["password"]; 
+        $confirmPassword = $_POST["confirmPassword"]; 
+        
+          // 1. make sure that username is not empty
+          if (empty($username))
+          {
+              apologize("please, provide a username entry !!!"); 
+          }
+          else if (empty($password))
+          {
+               apologize("please, provide a password entry !!!"); 
+          }
+          else if ($password !== $confirmPassword)
+          {
+              apologize("password doens't match"); 
+          }
+         
           // 4. make sure that username and email address is not already used if its not there then
-          //    1. insert and redirect to index.html 
+          //    1. insert and redirect to index.html
+          $result = CS50::query("INSERT IGNORE INTO users (username, hash, cash) VALUES(?, ?, 10000.0000)",
+            $_POST["username"], password_hash($_POST["password"], PASSWORD_DEFAULT));
+          
+          
           // else 
-          //    apologize 
+          //    apologize
+     
+ 
+          
     }
 
 ?>
