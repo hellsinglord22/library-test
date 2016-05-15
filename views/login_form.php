773657fd26c25b3eaf0c3f2ebc27d0769590f626
Hesham Adel <heshascorpe55@gmail.com>
2016-05-15 09:35:39 +0000

    make login and register design

--- views/login_form.php
+++ views/login_form.php
@@ -1,27 +1,54 @@
 <style>
+    @import url(http://fonts.googleapis.com/earlyaccess/droidarabickufi.css);
+    
+    form
+    {
+        margin-left: 200px;
+        margin-right: 200px;
+        margin-top: 50px;    
+    }
+    
     body
     {
-        margin: 500px;
-        margin-top: 50px;    
+        direction: rtl;
+        font-family: 'Droid Arabic Naskh', serif;
+        background-image: url("../images/callout_bg.jpg");
+    }
+    
+     h1
+    {
+        background:#222;
+        color:#f9f9f9;
+        padding:50px;
+        
+    }
+    
+    #bottom
+    {
+        margin-left: 200px;
+        margin-right: 200px;
     }
 </style>
 
+<h1>
+    تسجيل دخول 
+</h1>
 <form action="login.php" method="POST">
     <fieldset>
         <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="كلمة مستخدم " type="text"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="password" placeholder="Password" type="password"/>
+            <input class="form-control" name="password" placeholder="كلمة مرور " type="password"/>
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
                 <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
-                Log In
+                تسجيل دخول 
             </button>
         </div>
     </fieldset>
 </form>
-<div>
-    or <a href="register.php">register</a> for an account
+<div id="bottom">
+    أو  <a href="register.php">أنشاء حساب </a> 
 </div>
--- views/register_form.php
+++ views/register_form.php
@@ -1,29 +1,58 @@
 <style>
-    body
+    @import url(http://fonts.googleapis.com/earlyaccess/droidarabickufi.css);
+
+    form
     {
-        margin: 500px;
+        margin-left: 200px;
+        margin-right: 200px;
         margin-top: 50px;    
     }
+    
+    body
+    {
+        direction: rtl;
+        font-family: 'Droid Arabic Naskh', serif;
+        background-image: url("../images/callout_bg.jpg");
+        
+    }
+    
+     h1
+    {
+        background:#222;
+        color:#f9f9f9;
+        padding:50px;
+        
+    }
+    
+    #bottom
+    {
+        margin-left: 200px;
+        margin-right: 200px;
+    }
 </style>
+
+<h1>
+     إنشاء حساب 
+</h1>
 <form action="register.php" method="post">
     <fieldset>
         <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="كلمة مستخدم " type="text"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="password" placeholder="Password" type="password"/>
+            <input class="form-control" name="password" placeholder="كلمة مرور " type="password"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="confirmPassword" placeholder="confirm password" type="password"/>
+            <input class="form-control" name="confirmPassword" placeholder="أدخل كلمة المرور مرة أخرى " type="password"/>
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
                 <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
-                register
+                إنشاء حساب 
             </button>
         </div>
     </fieldset>
 </form>
-<div>
-    or <a href="login.php">login</a> for an account
+<div id="bottom">
+    أو <a href="login.php">سجل دخولك </a>
 </div>
