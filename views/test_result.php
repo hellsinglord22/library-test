6947b17477ee267fa04b9f50411fee78609cd484
H <heshamscorpe55@gmail>
2016-05-22 11:46:46 +0200

    fix result page button

--- views/test_result.php
+++ views/test_result.php
@@ -15,7 +15,7 @@
         background-image: url("../images/callout_bg.jpg");
     }
     
-     h1
+    h1
     {
         background:#222;
         color:#f9f9f9;
@@ -42,45 +42,45 @@
     <h1>
         <?php
         
-            print('ﻧﺘﻴﺠﺔ ﺇﺧﺘﺒﺎﺭ اﻟﻤﻮﺩﻳﻮﻝ ');
-            if ($current_module == 1)
-            {
-                print('اﻷﻭﻝ '); 
-            }
-            else if ($current_module == 2)
-            {
-                print('اﻟﺜﺎﻧﻲ '); 
-            }
-            else if ($current_module == 3)
-            {
-                print('اﻟﺜﺎﻟﺚ '); 
-            }
+        print('ﻧﺘﻴﺠﺔ ﺇﺧﺘﺒﺎﺭ اﻟﻤﻮﺩﻳﻮﻝ ');
+        if ($current_module == 1)
+        {
+            print('اﻷﻭﻝ '); 
+        }
+        else if ($current_module == 2)
+        {
+            print('اﻟﺜﺎﻧﻲ '); 
+        }
+        else if ($current_module == 3)
+        {
+            print('اﻟﺜﺎﻟﺚ '); 
+        }
         ?>
     </h1>
     
-        
+    
     <div>
         <?php
         
             // Attributes // 
-            $fail = "ﻋﺰﻳﺰﻱ اﻟﻤﺘﻌﻠﻢ ﻟﻘﺪ ﺣﺼﻠﺖ ﻋﻠﻰ ﺩﺭﺟﺔ ﺃﻗﻞ ﻣﻦ 90 % ﻗﻢ ﺑﺎﻟﻀﻐﻂ ﻋﻠﻰ اﻟﺘﺎﻟﻲ ﻟﺪﺭاﺳﺔ اﻟﻤﻮﺩﻳﻞ ﻣﺮﺓ ﺃﺧﺮﻯ  "; 
-            $success = "ﻋﺰﻳﺰﻱ اﻟﻤﺘﻌﻠﻢ ﻟﻘﺪ ﺣﺼﻠﺖ ﻋﻠﻰ ﺩﺭﺟﺔ ﺃﻋﻠﻰ ﻣﻦ 90 ٪ ﺇﺿﻐﻂ ﻋﻠﻰ اﻟﺘﺎﻟﻲ ﻟﻠﺬﻫﺎﺏ ﻟﻠﺼﻔﺤﺔ اﻟﺮﺋﻴﺴﻴﺔ "; 
-            
-            
-            $newline = "</br>"; 
-            
-            
-            
-            // end of Attributes // 
-            
+        $fail = "ﻋﺰﻳﺰﻱ اﻟﻤﺘﻌﻠﻢ ﻟﻘﺪ ﺣﺼﻠﺖ ﻋﻠﻰ ﺩﺭﺟﺔ ﺃﻗﻞ ﻣﻦ 90 % ﻗﻢ ﺑﺎﻟﻀﻐﻂ ﻋﻠﻰ اﻟﺘﺎﻟﻲ ﻟﺪﺭاﺳﺔ اﻟﻤﻮﺩﻳﻞ ﻣﺮﺓ ﺃﺧﺮﻯ  "; 
+        $success = "ﻋﺰﻳﺰﻱ اﻟﻤﺘﻌﻠﻢ ﻟﻘﺪ ﺣﺼﻠﺖ ﻋﻠﻰ ﺩﺭﺟﺔ ﺃﻋﻠﻰ ﻣﻦ 90 ٪ ﺇﺿﻐﻂ ﻋﻠﻰ اﻟﺘﺎﻟﻲ ﻟﻠﺬﻫﺎﺏ ﻟﻠﺼﻔﺤﺔ اﻟﺮﺋﻴﺴﻴﺔ "; 
         
-            if ($current_module == 1)
+        
+        $newline = "</br>"; 
+        
+        
+        
+            // end of Attributes // 
+        
+        
+        if ($current_module == 1)
+        {
+            if ($_SESSION['module1'] >= 90)
             {
-                if ($_SESSION['module1'] >= 90)
-                {
-                 print($success);    
-                 print($newline); 
-                 print('
+               print($success);    
+               print($newline); 
+               print('
                 <div class="message">
                     <a href="http://rdatrainingcourse.com/module1/story.html">
                         <button>
@@ -88,84 +88,84 @@
                         </button>
                     </a>        
                 </div>'); 
-                }
-                else 
-                {
-                    print($fail); 
-                    print($newline); 
-                    print('
-                        <div class="message">
-                            <button>
-                                <a href="http://rdatrainingcourse.com/contentm1/story.html">
-                                    اﻟﺘﺎﻟﻲ 
-                                </a>
-                            </button>
-                        </div>'); 
-                }
-                
-            }
-            else if ($current_module == 2)
-            {
-                if ($_SESSION['module2'] >= 90)
-                {
-                    print($success);    
-                    print($newline); 
-                    print('
+           }
+           else 
+           {
+            print($fail); 
+            print($newline); 
+            print('
                 <div class="message">
-                    <button>
-                        <a href="http://rdatrainingcourse.com/module1/story.html">
+                    <a href="http://rdatrainingcourse.com/contentm1/story.html">
+                        <button>
                             اﻟﺘﺎﻟﻲ 
-                        </a>
-                    </button>
+                        </button>
+                    </a>
                 </div>'); 
-                    
-                }
-                else 
-                {
-                    print($fail); 
-                    print($newline); 
-                    print('
-                        <div class="message">
-                            <button>
-                                <a href="http://rdatrainingcourse.com/contentm2/story.html">
-                                    اﻟﺘﺎﻟﻲ 
-                                </a>
-                            </button>
-                        </div>');
-                }
-            }
-            else if ($current_module == 3)
-            {
-                if ($_SESSION['module3'] >= 90)
-                {
-                    print($success);    
-                    print($newline); 
-                    print('
+        }
+        
+    }
+    else if ($current_module == 2)
+    {
+        if ($_SESSION['module2'] >= 90)
+        {
+            print($success);    
+            print($newline); 
+            print('
                 <div class="message">
-                    <button>
-                        <a href="http://rdatrainingcourse.com/module1/story.html">
+                    <a href="http://rdatrainingcourse.com/module1/story.html">
+                        <button>
                             اﻟﺘﺎﻟﻲ 
-                        </a>
-                    </button>
+                        </button>
+                    </a>
                 </div>'); 
-                    
-                }
-                else 
-                {
-                    print($fail);
-                    print($newline); 
-                    print('
-                        <div class="message">
-                            <button>
-                                <a href="http://rdatrainingcourse.com/contentm3/story.html">
-                                    اﻟﺘﺎﻟﻲ 
-                                </a>
-                            </button>
-                        </div>'); 
-                }
-            }
-        ?>
-    </div>
-    
-    
+            
+        }
+        else 
+        {
+            print($fail); 
+            print($newline); 
+            print('
+                <div class="message">
+                    <a href="http://rdatrainingcourse.com/contentm2/story.html">
+                        <button>
+                            اﻟﺘﺎﻟﻲ 
+                        </button>
+                    </a>
+                </div>');
+        }
+    }
+    else if ($current_module == 3)
+    {
+        if ($_SESSION['module3'] >= 90)
+        {
+            print($success);    
+            print($newline); 
+            print('
+                <div class="message">
+                    <a href="http://rdatrainingcourse.com/module1/story.html">
+                        <button>
+                            اﻟﺘﺎﻟﻲ 
+                        </button>
+                    </a>
+                </div>'); 
+            
+        }
+        else 
+        {
+            print($fail);
+            print($newline); 
+            print('
+                <div class="message">
+                    <a href="http://rdatrainingcourse.com/contentm3/story.html">
+                        <button>
+                            اﻟﺘﺎﻟﻲ 
+                        </button>
+                    </a>
+                </div>'); 
+        }
+    }
+    ?>
+</div>
+
+
 </div>
